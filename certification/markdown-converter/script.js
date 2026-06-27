a27c459bb4c844b15fc8191a635bdb31fbe6f208
Dex <a.markdexter@gmail.com>
2026-06-26 23:22:43 -0700

    adding regex

--- certification/markdown-converter/script.js
+++ certification/markdown-converter/script.js
@@ -1,10 +1,22 @@
 const markDownInput = document.getElementById("markdown-input");
 const htmlOutput = document.getElementById("html-output");
-const previewEl =  document.getElementById("preview");
+const previewEl = document.getElementById("preview");
 
 const convertMarkdown = () => {
-  const phrase = markDownInput.value;
-  console.log(phrase);
+  const markdownEl = markDownInput.value;
+  const result = markdownEl
+    .replace(/^#\s+(.+)/gm, "<h1>$1</h1>")
+    .replace(/^##\s+(.+)/gm, "<h2>$1</h2>")
+    .replace(/^###\s+(.+)/gm, "<h3>$1</h3>")
+    .replace(/^\*\*(.+)\*\*$/gm, "<strong>$1</strong>")
+    .replace(/^__(.+)__/gm, "<strong>$1</strong>")
+
+  return result
 }
 
-markDownInput.addEventListener("change", convertMarkdown);
+
+markDownInput.addEventListener("input", () => {
+  const result = convertMarkdown();
+  htmlOutput.textContent = result;
+  previewEl.innerHTML = result;
+});
