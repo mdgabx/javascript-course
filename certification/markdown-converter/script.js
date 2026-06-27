a02e801479f86ded85ff9ee48e477ecbd92d55a5
Dex <a.markdexter@gmail.com>
2026-06-27 05:07:47 -0700

    certification markdown done

--- certification/markdown-converter/script.js
+++ certification/markdown-converter/script.js
@@ -8,8 +8,11 @@ const convertMarkdown = () => {
     .replace(/^#\s+(.+)/gm, "<h1>$1</h1>")
     .replace(/^##\s+(.+)/gm, "<h2>$1</h2>")
     .replace(/^###\s+(.+)/gm, "<h3>$1</h3>")
-    .replace(/^\*\*(.+)\*\*$/gm, "<strong>$1</strong>")
-    .replace(/^__(.+)__/gm, "<strong>$1</strong>")
+    .replace(/(?:\*\*|__)(.+)(?:\*\*|__)/gm, "<strong>$1</strong>")
+    .replace(/(?:\*|_)(.+)(?:\*|_)/gm, "<em>$1</em>")
+    .replace(/!\[(.+)\]\((.+)\)/gm, "<img alt=$1 src=$2 />")
+    .replace(/\[(.+)\]\((.+)\)/gm, "<a href=$2>$1</a>.")
+    .replace(/^(?:>)\s(.+)$/gm, "<blockquote>$1</blockquote>")
 
   return result
 }
