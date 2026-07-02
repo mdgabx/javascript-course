c6829b6e461f71302374e2aadefddcd37af43ad0
Dex <a.markdexter@gmail.com>
2026-07-02 02:47:04 -0700

    completed the form validationcourse

--- form-validation/complaint-form/script.js
+++ form-validation/complaint-form/script.js
@@ -8,6 +8,11 @@ const complaint = document.getElementById("complaints-group");
 const complaintGroups = document.querySelectorAll('#complaints-group input[type="checkbox"]');
 const otherComplaint = document.getElementById("other-complaint");
 const complaintDescription = document.getElementById("complaint-description");
+const solutionsGroups = document.querySelectorAll('#solutions-group input[type="radio"]')
+const solutionsContainer = document.getElementById('solutions-group');
+const otherSolutions = document.getElementById('other-solution')
+const solutionDescription = document.getElementById('solution-description');
+
 
 function validateForm() {
   const name = fullName.value;
@@ -22,8 +27,12 @@ function validateForm() {
   const isValidQuantity = /^\d+$/.test(quantity) && Number(quantity) > 0;
   const hasComplaints = Array.from(complaintGroups).some((complaint) => complaint.checked);
   const checkedOtherComplaint = otherComplaint.checked ? true : false;
-
   const complaintDescriptionValid = isValidComplaintDescription(checkedOtherComplaint);
+  const hasSolutions = Array.from(solutionsGroups).some(solution => solution.checked)
+  const checkedOtherSolutions = otherSolutions.checked ? true : false
+  const solutionsValid = isValidSolution(checkedOtherSolutions);
+  
+
 
   return {
     "full-name": name === "" ? false : true,
@@ -32,10 +41,20 @@ function validateForm() {
     "product-code": isValidProductCode,
     "quantity": isValidQuantity,
     "complaints-group": hasComplaints,
-    "complaint-description": complaintDescriptionValid  
+    "complaint-description": complaintDescriptionValid,
+    "solutions-group": hasSolutions,
+    "solution-description": solutionsValid
   }
 }
 
+
+function isValidSolution(checkedSolution) {
+  if(!checkedSolution) return true;
+
+  if(checkedSolution && solutionDescription.value.length < 20) { return false};
+
+  return checkedSolution
+}
 
 function isValidComplaintDescription (checkedComplaint) {
   if(!checkedComplaint) {
@@ -44,16 +63,13 @@ function isValidComplaintDescription (checkedComplaint) {
 
   if(checkedComplaint && complaintDescription.value.length < 20) {
     return false;
-  }
+  } 
 
-  return checkedComplaint
+  return checkedComplaint;
 }
-
-
 
 function handleInputChange() {
   const form = validateForm();
-  console.log(form);
 
   fullName.style.borderColor = form["full-name"] ? "green" : "red";
   emailInput.style.borderColor = form["email"] ? "green" : "red";
@@ -62,6 +78,9 @@ function handleInputChange() {
   productCodeInput.style.borderColor = form["product-code"] ? "green" : "red";
   quantityInput.style.borderColor = form["quantity"] ? "green" : "red";
   complaint.style.borderColor = form["complaints-group"] ? "green" : "red";
+  complaintDescription.style.borderColor = form["complaint-description"] ? "green" : "red";
+  solutionsContainer.style.borderColor = form['solutions-group'] ? "green" : "red";
+  solutionDescription.style.borderColor = form['solution-description'] ? "green": "red"
 }
 
 fullName.addEventListener("change", handleInputChange)
@@ -72,3 +91,20 @@ quantityInput.addEventListener("change", handleInputChange)
 complaintGroups.forEach(checkbox => {
   checkbox.addEventListener("change", handleInputChange)
 })
+complaintDescription.addEventListener("change", handleInputChange)
+solutionsGroups.forEach((radio => {
+  radio.addEventListener("change", handleInputChange);
+}))
+solutionDescription.addEventListener("change", handleInputChange)
+
+
+function isValid(formResult) {
+  return Object.values(formResult).every((value) => value === true);
+}
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  isValid(validateForm());
+});
