821823132ea7bb31a8a8bc2196c9b9193271f6e2
Dex <a.markdexter@gmail.com>
2026-07-24 02:28:30 -0700

    completed the bank account certification exam

--- classes/bank-account-management/script.js
+++ classes/bank-account-management/script.js
@@ -5,31 +5,58 @@ class BankAccount {
   }
 
   deposit(amount) {
-    if(amount <= 0) {
+    if (amount <= 0) {
       return "Deposit amount must be greater than zero."
     }
 
-    this.transactions.push(amount);
+    this.transactions.push({
+      amount: amount,
+      type: "deposit"
+    });
+
     this.balance += amount;
     return `Successfully deposited $${amount}. New balance: $${this.balance}`;
   }
 
   withdraw(amount) {
+    if (amount <= 0 || amount >= this.balance) {
+      return "Insufficient balance or invalid amount.";
+    }
 
+    this.transactions.push({
+      amount: amount,
+      type: "withdraw"
+    })
+
+    this.balance -= amount;
+    return `Successfully withdrew $${amount}. New balance: $${this.balance}`;
   }
 
   checkBalance() {
-
+    return `Current balance: $${this.balance}`;
   }
 
-  listAllDeposits () {
+  listAllDeposits() {
+    const deposits =  this.transactions.filter((transaction) => transaction.type === "deposit")
+    .map((transaction) => transaction.amount)
 
+    return `Deposits: ${deposits.join(",")}`;
   }
 
-  listAllWithdrawals () {
+  listAllWithdrawals() {
+    const withdrawals = this.transactions.filter((transaction) => transaction.type === "withdraw").map((transaction) => transaction.amount)
 
+    return `Withdrawals: ${withdrawals.join(",")}`
   }
 }
 
-const transact = new BankAccount();
-console.log(transact.deposit(100));
+const myAccount  = new BankAccount();
+myAccount.deposit(100)
+myAccount.deposit(350)
+myAccount.deposit(90)
+myAccount.withdraw(20)
+myAccount.withdraw(50)
+myAccount.withdraw(100)
+console.log(myAccount.listAllDeposits());
+console.log(myAccount.listAllWithdrawals());
+console.log(myAccount.checkBalance())
