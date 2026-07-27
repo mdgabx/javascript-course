c9b063762d047fbf7aa9460628d8bbb26127354d
Dex <a.markdexter@gmail.com>
2026-07-27 05:40:23 -0700

    adding the decimaltobinary function

--- recursion/decimal-t--binary/script.js
+++ recursion/decimal-t--binary/script.js
@@ -1,9 +1,22 @@
+
+
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  return "1010".toString(2);
+  let binary = "";
+
+  if (input === 0) {
+    binary = "0";
+  }
+
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+  }
+
+  result.innerText = binary;
 };
 
 const checkUserInput = () => {
