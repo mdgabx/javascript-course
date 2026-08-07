91d223a1f376da257d48452cfdf67d0704dae7d5
Dex <a.markdexter@gmail.com>
2026-08-06 21:08:52 -0700

    stack lab completed

--- data-structures/stacked/fifo.js
+++ data-structures/stacked/fifo.js
@@ -8,9 +8,31 @@ const push = (stack, element) => {
   stack.collection.push(element)
 }
 
+const pop = (stack) => {
+  return stack.collection.pop()
+}
+
+const peek = (stack) => {
+
+  if(!stack.collection.length > 0) {
+    return undefined
+  } 
+
+  return stack.collection[stack.collection.length - 1]
+}
+
+const isEmpty = (stack) => {
+  return stack.collection.length > 0 ? false : true
+}
+
+const clear = (stack) => {
+  return stack.collection = [];
+}
+
 const myStack = initStack();
 
 push(myStack, 2);
 push(myStack, 3);
 push(myStack, 6);
+console.log(pop(myStack));
 console.log(myStack)
