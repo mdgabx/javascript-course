aa8c2af8705c0ff2df02a796c5a05186da977260
Dex <a.markdexter@gmail.com>
2026-08-31 03:39:16 -0700

    Initial

---
+++ graphs-and-trees/shortest-path/index.js
@@ -0,0 +1,13 @@
+const INF = Infinity;
+const adjMatrix = [
+  [0, 5, 3, INF, 11, INF],
+  [5, 0, 1, INF, INF, 2],
+  [3, 1, 0, 1, 5, INF],
+  [INF, INF, 1, 0, 9, 3],
+  [11, INF, 5, 9, 0, INF],
+  [INF, 2, INF, 3, INF, 0],
+];
+
+function shortestPath(matrix, startNode, targetNode = null) {
+  const n = matrix.length  
+}
