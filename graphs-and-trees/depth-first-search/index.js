e9266345a04d93baabaf602d8127be3b9d5a6e20
Dex <a.markdexter@gmail.com>
2026-09-06 02:22:56 -0700

    dfs implementation

--- graphs-and-trees/depth-first-search/index.js
+++ graphs-and-trees/depth-first-search/index.js
@@ -1,7 +1,7 @@
 const matrixGraph = [
-  [0, 1, 0, 0], 
-  [1, 0, 1, 0], 
-  [0, 1, 0, 1], 
+  [0, 1, 0, 0],
+  [1, 0, 1, 0],
+  [0, 1, 0, 1],
   [0, 0, 1, 0]
 ]
 
@@ -9,11 +9,27 @@ function dfs(graph, root) {
   const n = graph.length;
   const visited = new Array(n).fill(false)
   const stack = [root];
+  const result = [];
 
-  while(stack.length > 0) {
-    
-  } 
+  while (stack.length > 0) {
+    const current = stack.pop()
 
+    if (visited[current]) {
+      continue
+    }
+
+    visited[current] = true;
+    result.push(current);
+
+    for (let neighbor = 0; neighbor < n; neighbor++) {
+      // graph[current][neighbor] === 1 means there's an edge to that neighbor
+      if (graph[current][neighbor] === 1 && !visited[neighbor]) {
+        stack.push(neighbor); // add it to the stack to explore later
+      }
+    }
+  }
+
+  return result;
 }
 
 dfs(matrixGraph, 1);
