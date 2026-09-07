59d623205372f3dda0e084c6c9b444ec42eb1471
Dex <a.markdexter@gmail.com>
2026-09-07 06:29:15 -0700

    n queen

---
+++ graphs-and-trees/n-queen/index.js
@@ -0,0 +1,15 @@
+function dfsNQueens(n) {
+  if(n < 1) return [];
+
+  let solutions = [];
+  let cols = new Set();
+  let diag1 = new Set();
+  let diag2 = new Set(); 
+  let placement = new Array(n).fill(-1);
+
+  function backtrack(row) {
+    
+  }
+}
+
+dfsNQueens(1);
