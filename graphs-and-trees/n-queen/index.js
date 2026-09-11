13df6350db8e5bf45ba7aa3af913a3cb21146cb3
Dex <a.markdexter@gmail.com>
2026-09-10 18:43:37 -0700

    n-queen

--- graphs-and-trees/n-queen/index.js
+++ graphs-and-trees/n-queen/index.js
@@ -2,14 +2,36 @@ function dfsNQueens(n) {
   if(n < 1) return [];
 
   let solutions = [];
-  let cols = new Set();
-  let diag1 = new Set();
-  let diag2 = new Set(); 
-  let placement = new Array(n).fill(-1);
 
-  function backtrack(row) {
-    
+  function isValid(queens, row, col) {
+    for(let i = 0; i < row; i++) {
+      const qCol = queens[i];
+
+      if(qCol === col || Math.abs(qCol - col) === row - i) {
+        return false;
+      }
+    }
+
+    return true;
   }
+
+  function dfs(row, queens) {
+    if(row === n) {
+      solutions.push([...queens]);
+      return;
+    }
+
+    for(let col = 0; col < n; col++) {
+      if(isValid(queens, row, col)) {
+        queens[row] = col;
+        dfs(row + 1, queens);
+      }
+    }
+  }
+
+  dfs(0, []);
+  return solutions;
+
 }
 
 dfsNQueens(1);
