b8080734d88368bb29df079f5af5f903fe8e540d
Dex <a.markdexter@gmail.com>
2026-09-13 00:30:41 -0700

    adding helper function

--- functional-programming/sorting-visualizer/script.js
+++ functional-programming/sorting-visualizer/script.js
@@ -5,7 +5,7 @@ const generateElement = () => {
 const generateArray = () => {
   const randomArray = [];
   let index = 0;
-  
+
   while (index < 5) {
     const randomInteger = generateElement();
     randomArray.push(randomInteger)
@@ -26,4 +26,19 @@ const fillArrContainer = (element, integers) => {
     element.appendChild(newSpan);
   });
 }
+
+const isOrdered = (a, b) => {
+  return a <= b ? true : false;
+}
+
+const swapElements = (integers, index) => {
+  if (!isOrdered(integers[index], integers[index + 1])) {
+    const temp = integers[index];
+    integers[index] = integers[index + 1];
+    integers[index + 1] = temp;
+  }
+}
+
+
+
 
